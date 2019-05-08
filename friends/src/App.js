dd11f8ead3c2e2d6ca0c0170543e91725e0915f6
Mylynh Nguyen <34496865+nmylynh@users.noreply.github.com>
2019-05-08 16:26:41 -0700

    fetched data, set state to fetched data, console.log to figure out if it worked

--- friends/src/App.js
+++ friends/src/App.js
@@ -7,7 +7,16 @@ export class App extends Component {
   state = {
     friends: []
   }
+
+  componentDidMount() {
+    axios
+      .get('http://localhost:5000/friends')
+      .then(response => this.setState({ friends: response.data }))
+      .catch(error => console.log(error));
+  }
+
   render() {
+    console.log(this.state.friends);
     return (
       <div>
         

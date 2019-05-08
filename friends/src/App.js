b2a656be1846746eb07b5d06927c7a86c9765cae
Mylynh Nguyen <34496865+nmylynh@users.noreply.github.com>
2019-05-08 16:02:10 -0700

    creating app, ignore

---
+++ friends/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+
+
+export class App extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default App;

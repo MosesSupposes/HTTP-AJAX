d2286462e9f1cb698a4d8b958ab0975b352af51b
Mylynh Nguyen <34496865+nmylynh@users.noreply.github.com>
2019-05-08 19:44:17 -0700

    map over each object in the array to pass as props into the friend component

--- friends/src/components/Friendslist.js
+++ friends/src/components/Friendslist.js
@@ -1,13 +1,16 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
-export class Friendslist extends Component {
-  render() {
+
+const Friendslist = props => {
     return (
-      <div>
-        
-      </div>
+        <div className="friends-container">
+            {props.friends.map(friend) => {
+                return <Friend key={friend.id} friend = {friend} name={friend.name} age={friend.age} email={friend.email} />}
+            }
+        </div>
     )
-  }
 }
+
 
 export default Friendslist;
